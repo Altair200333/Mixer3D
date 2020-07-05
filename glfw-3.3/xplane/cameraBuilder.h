179e0b7de6cbddf6501e7e62d3976919c0749fe3
Altair <altair200333@yandex.ru>
2020-07-05 14:17:37 +0700

    camera orientation save

--- glfw-3.3/xplane/cameraBuilder.h
+++ glfw-3.3/xplane/cameraBuilder.h
@@ -23,6 +23,14 @@ inline void CameraBuilder::toJson(Object* object, nlohmann::json& value)
 	auto transform = object->getComponent<Transform>();
 	value["position"] = { transform->position.x, transform->position.y, transform->position.z };
 	value["fov"] = object->getComponent<Camera>()->zoom;
+	auto front = object->getComponent<Camera>()->Front;
+	value["front"] = {front.x, front.y, front.z};
+	auto right = object->getComponent<Camera>()->Right;
+	value["right"] = { right.x, right.y, right.z };
+	auto up = object->getComponent<Camera>()->Up;
+	value["up"] = { up.x, up.y, up.z };
+	value["yaw"] = object->getComponent<Camera>()->yaw;
+	value["pitch"] = object->getComponent<Camera>()->pitch;
 }
 
 inline Object* CameraBuilder::fromJson(nlohmann::json& value, Scene& scene)
@@ -31,5 +39,11 @@ inline Object* CameraBuilder::fromJson(nlohmann::json& value, Scene& scene)
 	glm::vec3 position = getVec3Or(value, "position", { 0,0,-1 });
 	obj->addComponent(new Transform(obj, position))->addComponent(new Camera(obj, static_cast<float>(scene.window->width) / scene.window->height,
 		static_cast<float>(value.at("fov"))));
+	auto camera = obj->getComponent<Camera>();
+	camera->Front = getVec3Or(value, "front", camera->Front);
+	camera->Right = getVec3Or(value, "right", camera->Right);
+	camera->Up = getVec3Or(value, "up", camera->Up);
+	camera->yaw = getFloatOr(value, "yaw", camera->yaw);
+	camera->pitch = getFloatOr(value, "pitch", camera->pitch);
 	return obj;
 }
